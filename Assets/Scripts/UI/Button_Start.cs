b7910631fe09e603ad7747d46355ab5364d155d5
Akmulla <d4n73@yandex.ru>
2017-02-08 01:10:09 +0600

    Start_Menu

---
+++ Assets/Scripts/UI/Button_Start.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Button_Start : MonoBehaviour
+{
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}

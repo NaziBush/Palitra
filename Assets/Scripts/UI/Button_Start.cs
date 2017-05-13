9a4aeba0ef73bd7686cde53aa10eaa0b11de0726
Akmulla <d4n73@yandex.ru>
2017-05-14 03:08:15 +0600

    v 6.2
    new grafen

--- Assets/Scripts/UI/Button_Start.cs
+++ Assets/Scripts/UI/Button_Start.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Button_Start : MonoBehaviour
 {
 
-	public void StartGame()
+	public void BeginGame()
     {
         SceneManager.LoadScene("Main");
     }
